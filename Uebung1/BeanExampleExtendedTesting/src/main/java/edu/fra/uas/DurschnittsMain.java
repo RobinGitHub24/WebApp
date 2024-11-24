763c9847cbb2ffd66461fe050b8c7231bfc5417a
RobinGitHub24 <robin.schaefer1@outlook.de>
2024-11-24 17:07:57 +0100

    Neustart für übung

--- Uebung1/BeanExampleExtendedTesting/src/main/java/edu/fra/uas/DurschnittsMain.java
+++ Uebung1/BeanExampleExtendedTesting/src/main/java/edu/fra/uas/DurschnittsMain.java
@@ -1,10 +1,9 @@
 package edu.fra.uas;
 
-import edu.fra.uas.model.Student;
 import edu.fra.uas.model.KursNote;
 import edu.fra.uas.service.StudentService;
+
 import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,33 +16,21 @@ public class DurschnittsMain implements CommandLineRunner {
     @Autowired
     private StudentService studentService;
 
-    private Student student; // Ein einzelner Student
-
     public static void main(String[] args) {
         SpringApplication.run(DurschnittsMain.class, args);
     }
 
     @PostConstruct
     public void init() {
-        // Initialisierung des Studenten
-        student = new Student("Max Mustermann");
-        student.addNote(new KursNote("Mathematik", 2.0));
-        student.addNote(new KursNote("Informatik", 1.7));
-        student.addNote(new KursNote("Physik", 2.3));
-        System.out.println("Student initialisiert: " + student.getName());
+        studentService.getStudent().addNote(new KursNote("Mathematik", 2.0));
+        studentService.getStudent().addNote(new KursNote("Informatik", 1.7));
+        studentService.getStudent().addNote(new KursNote("Physik", 2.3));
+        System.out.println("Student initialisiert: " + studentService.getStudent().getName());
     }
 
     @Override
     public void run(String... args) {
-        // Berechnung des Durchschnitts für den initialisierten Studenten
-        double durchschnitt = studentService.berechneDurchschnitt(student);
-        System.out.println("Der Notendurchschnitt von " + student.getName() + " beträgt: " + durchschnitt);
-    }
-
-    @PreDestroy
-    public void cleanup() {
-        // Bereinigung der Daten
-        student = null;
-        System.out.println("Studentendaten wurden gelöscht.");
+        double durchschnitt = studentService.getStudent().berechneDurchschnitt();
+        System.out.println("Der Notendurchschnitt beträgt: " + durchschnitt);
     }
 }
